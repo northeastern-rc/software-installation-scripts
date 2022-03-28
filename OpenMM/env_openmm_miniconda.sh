a7cd98315ff5710266f5f67e434ac89cc3b66608
marianalevineu <m.levi@northeastern.edu>
2022-03-28 11:45:33 -0400

    added OpenMM scripts

---
+++ OpenMM/env_openmm_miniconda.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## This script sources the OpenMM environemnt to install and run OpenMM.
+## Usage: source env_openmm_miniconda.sh
+
+## Define your OpenMM directory. The OpenMM install directory is set to: $OPEMMDIR/7.5.
+export OPEMMDIR=$HOME/openmm
+export VERSION=7.5
+## Provide your miniconda3/anaconda3 local build:
+export MYCONDA_DIR=$HOME/miniconda3
+
+## Load libraries:
+module load gcc/10.1.0 openmpi/4.1.0-gcc10.1-cuda11.2 cuda/11.2
+
+##Activate Conda/python env:
+source $MYCONDA_DIR/bin/activate
+export PYTHON=`which python3.8`
+export prefixName=$OPEMMDIR/$VERSION
+export SWIGDIR=$OPEMMDIR/swig-3.0.5
+
+export PATH=$SWIGDIR/bin:$prefixName/bin:$PATH
+export LD_LIBRARY_PATH=$prefixName/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$prefixName/lib:$LIBRARY_PATH
+export CPATH=$prefixName/include:$CPATH
---
+++ OpenMM/install_openmm_miniconda.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#SBATCH -N 1 
+#SBATCH -n 10
+#SBATCH -p gpu
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH -t 04:00:00
+#SBATCH --output=openmm_install.out
+#SBATCH --error=openmm_install.err
+
+#====================================================================================
+#title           :install_openmm_miniconda.sh
+#description     :This script will install the parallel OpenMM - version 7.5. With GPU support and Miniconda. Build script for the Skylake AVX512 arch.
+#author          :Mariana Levi
+#date            :March 2022
+#version         :0.1    
+#usage           :sbatch install_openmm_miniconda.sh
+#notes           :Installs OpenMM after dependencies were installed. Uses Miniconda + CUDA. Will install for v100-sxm2 GPU types.
+#bash_version    :4.2.46(2)-release (x86_64-redhat-linux-gnu)
+#====================================================================================
+
+set -e
+
+## Set the software environment:
+source env_openmm_miniconda.sh
+
+mkdir -p $OPEMMDIR
+
+## set up miniconda3 virtual environment for openmm:
+conda update conda -y ##comment this line out if using a module instead of your own miniconda3.
+conda create --name openmm python=3.8 -y
+conda activate openmm
+conda install -c anaconda numpy cython -y
+conda install cmake -y
+export PYTHON=`which python3.8`
+
+mkdir -p $OPEMMDIR/src
+cd $OPEMMDIR/src
+
+## swig 3 dependency installation:
+export SWIGDIR=$OPEMMDIR/swig-3.0.5
+wget https://prdownloads.sourceforge.net/swig/swig-3.0.5.tar.gz
+tar xvzf swig-3.0.5.tar.gz
+cd swig-3.0.5
+./configure 	--prefix=$SWIGDIR \
+		--with-perl5=/usr/bin/perl \
+		--with-python=$PYTHON \
+		--with-ruby=/usr/bin/ruby
+make -j
+make install
+
+export PATH=$SWIGDIR/bin:$PATH
+
+## Install OpenMM:
+cd $OPEMMDIR/src
+wget https://github.com/openmm/openmm/archive/refs/tags/7.5.0.tar.gz
+tar -zxvf 7.5.0.tar.gz
+cd openmm-7.5.0
+mkdir -p build_openmm
+cd build_openmm
+
+## Note - if building for another architecutre, make sure to modify this line:
+export myOPTflags="-O3 -march=skylake-avx512"
+
+cmake ../ -DOPENCL_LIBRARY=/shared/centos7/cuda/11.2/lib64/libOpenCL.so \
+	-DOPENMM_BUILD_CUDA_LIB=ON \
+	-DOPENMM_BUILD_OPENCL_LIB=OFF \
+	-DCUDA_HOST_COMPILER=/shared/centos7/gcc/10.1.0/bin/gcc \
+	-DCUDA_TOOLKIT_ROOT_DIR=/shared/centos7/cuda/11.2 \
+	-DCMAKE_INSTALL_PREFIX=$prefixName \
+	-DPYTHON_EXECUTABLE=$PYTHON \
+	-DCMAKE_C_FLAGS=$myOPTflags \
+	-DCMAKE_CXX_FLAGS=$myOPTflags \
+	-DCMAKE_Fortran_FLAGS=$myOPTflags \
+	-DMPI_INCLUDE_PATH=/shared/centos7/openmpi/4.1.0-gcc10.1-cuda11.2/include \
+	-DMPI_LIBRARIES=/shared/centos7/openmpi/4.1.0-gcc10.1-cuda11.2/lib \
+	-DCMAKE_INCLUDE_PATH=/shared/centos7/gcc/10.1.0/include:/shared/centos7/openmpi/4.1.0-gcc10.1-cuda11.2/include:/shared/centos7/cuda/11.2/include:$MYCONDA_DIR/include \
+	-DCMAKE_LIBRARY_PATH=/shared/centos7/gcc/10.1.0/lib:/shared/centos7/openmpi/4.1.0-gcc10.1-cuda11.2/lib:/shared/centos7/cuda/11.2/lib:$MYCONDA_DIR/lib:/shared/centos7/cuda/11.2/lib64
+
+make -j
+make install
+make PythonInstall
