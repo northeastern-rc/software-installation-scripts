1aced8c2bcb5505b1491e80355d91d85cc1e2f25
marianalevineu <m.levi@northeastern.edu>
2022-11-02 18:57:48 +0200

    updated README.md, install_relion.bash, relion-env.bash

--- Relion/install_relion.bash
+++ Relion/install_relion.bash
@@ -1,26 +1,32 @@
 #!/bin/bash
-set -e 
 #SBATCH -N 1
 #SBATCH -n 12
 #SBATCH -p gpu
 #SBATCH --gres=gpu:v100-sxm2:1
 #SBATCH --mem=10G
 #SBATCH -t 02:00:00
-#SBATCH -J install_relion_3.1.3
+#SBATCH -J install_relion
 
+## This script installs the RELION software version on Discovery, with GPU support.
+## Usage: sbatch install_relion.bash 
+## Note - make sure the file relion-env.bash is in the same location as this script.
+set -e 
+
+## Source environment variables and modules to install RELION:
 source relion-env.bash 
-SOFTWARE_DIR=${HOME}
-INSTALLATION_DIR=${mypath}
+
+## Download software:
 cd ${SOFTWARE_DIR}
 git clone https://github.com/3dem/relion.git
 cd relion
-git checkout master
-#git checkout 3.1.3
-mkdir build-3.1.3
-cd build-3.1.3
-
-
-CC=icc CXX=icpc cmake .. -DCUDA_ARCH=70 -DCMAKE_INSTALL_PREFIX=${INSTALLATION_DIR} -DMKLFFT=ON -DTIFF_LIBRARY=/shared/centos7/tiff/4.0.9/lib/libtiff.so -DTIFF_INCLUDE_DIR=/shared/centos7/tiff/4.0.9/include -DCUDA=ON -DCUDA_NVCC_FLAGS="-allow-unsupported-compiler"
-
+## Switch to the desired branch:
+git checkout ${RELION_VERSION}
+## Create a build directory:
+mkdir build-${RELION_VERSION}
+cd build-${RELION_VERSION}
+## Configure the build - using V100 GPU 70 ARCH, MKL support and MPI support:
+CC=icc CXX=icpc cmake .. -DCUDA_ARCH=70 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DMKLFFT=ON -DTIFF_LIBRARY=/shared/centos7/tiff/4.0.9/lib/libtiff.so -DTIFF_INCLUDE_DIR=/shared/centos7/tiff/4.0.9/include -DCUDA=ON -DCUDA_NVCC_FLAGS="-allow-unsupported-compiler"
+## Build the software:
 make -j12
+## Install it to the desired location:
 make install
--- Relion/relion-env.bash
+++ Relion/relion-env.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
-export mypath=$HOME/software/relion/3.1.3
-export PATH=$mypath/bin:$PATH
-export LD_LIBRARY_PATH=$mypath/lib:$LD_LIBRARY_PATH
+
+## This script will set all of the necessary environment varibales to build RELION.
+## Modify version as needed (note that installation steps may change accordingly:
+RELION_VERSION=3.1.3
+## Change the SOFTWARE_DIR path to where you'd like the software source code to be donwloaded:
+SOFTWARE_DIR=${HOME}/relion/src
+## Change the INSTALL_DIR to where you'd like the software binaries and libraries to be installed (should ideally be differnt than SOFTWARE_DIR.
+INSTALL_DIR=${HOME}/relion/${RELION_VERSION}
+
+## Load necessary modules which contain libraries and packages needed for the installation:
 module load intel/compilers-2021.2.0 openmpi/4.1.0-amd-intel2021 intel/mkl-2021.2.0 cuda/11.3 cmake/3.18.1
+
+## Set paths to binaries and libraries of the software (once built):
+export PATH=${INSTALL_DIR}/bin:$PATH
+export LD_LIBRARY_PATH=${INSTALL_DIR}/lib:$LD_LIBRARY_PATH
